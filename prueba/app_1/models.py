0191d1083bbad438c261b1ef794690b7d1483345
Leonardo Rodenas Escobar <leo.rodenas.e@gmail.com>
2023-07-11 10:47:08 -0400

    respaldo migracion productor

--- prueba/app_1/models.py
+++ prueba/app_1/models.py
@@ -30,3 +30,20 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre
     
+class Productor(models.Model):
+    idProductor = models.AutoField(primary_key=True)
+    nombreContacto = models.CharField(max_length=100, null=True, blank=True)
+    rut = models.CharField(max_length=15, null=True, blank=True)
+    razonSocial = models.CharField(max_length=100, null=True, blank=True)
+    direccion = models.CharField(max_length=100, null=True, blank=True)
+    comuna = models.CharField(max_length=100, null=True, blank=True)
+    rubro = models.CharField(max_length=100, null=True, blank=True)
+    
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
+
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        verbose_name_plural = "Productores"
