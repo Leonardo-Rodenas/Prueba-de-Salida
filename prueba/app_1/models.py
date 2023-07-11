f3ef39cd78e3e4cbccf20e60784acbc5047749e8
Leonardo Rodenas Escobar <leo.rodenas.e@gmail.com>
2023-07-11 13:52:23 -0400

    actualizo con migraciones nuevas de modelos

--- prueba/app_1/models.py
+++ prueba/app_1/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -63,3 +64,68 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nombreCliente
+       
+class Pedido(models.Model):
+    
+    PAGOS_CHOICES = [
+        ('Efectivo', 'Efectivo'),
+        ('Transferencia', 'Transferencia'),
+        ('Crédito', 'Crédito'),
+        ('Debito', 'Debito'),
+        ('PayPal', 'PayPal'),
+        ('Mach', 'Mach'),
+    ]
+
+    VIA_CHOICES = [
+        ('Telefono', 'Telefono'),
+        ('E-mail', 'E-mail'),
+        ('Web', 'Web'),
+    ]
+
+    ESTADOS_CHOICES = [
+        ('Pendiente', 'Pendiente'),
+        ('En Preparación', 'En Preparación'),
+        ('En Despacho', 'En Despacho'),
+        ('Entregado', 'Entregado'),
+        ('Cancelado', 'Cancelado'),
+    ]
+    
+    idcliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING, default=0)
+    metodo_pago = models.CharField(max_length=15, choices=PAGOS_CHOICES, default='Crédito')
+    fecha_pedido = models.DateTimeField(default=timezone.now)
+    mediopedido = models.CharField(max_length=15, choices=VIA_CHOICES, default='Web')
+    estado = models.CharField(max_length=15, choices=ESTADOS_CHOICES, default='Pendiente')
+    deleted = models.BooleanField(default=False)
+    pedido_staff = models.BooleanField(default=False)
+    is_modificable = models.BooleanField(default=True)
+    precio_total = models.IntegerField(default=0)
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
+
+    @property
+    def str_nombre(self):
+        return f"Pedido N°: {self.id}"
+
+    def __str__(self):
+        return self.str_nombre
+
+class DetallePedido(models.Model):
+    id = models.AutoField(primary_key=True)
+    idproducto = models.ForeignKey(Producto, on_delete=models.DO_NOTHING)
+    idpedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(null=False,default=1)
+    precio = models.IntegerField(null=False)
+    deleted = models.BooleanField(default=False)
+    
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
+
+    @property
+    def str_nombre(self):
+        return f"{self.id}, {self.idproducto}, {self.precio}"
+
+    def __str__(self):
+        return self.str_nombre
