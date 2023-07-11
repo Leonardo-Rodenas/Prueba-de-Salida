a44c2aac9ec09b5e5831e0a94b2659a7fd11da81
Leonardo Rodenas Escobar <leo.rodenas.e@gmail.com>
2023-07-11 11:01:05 -0400

    respaldo

--- prueba/app_1/admin.py
+++ prueba/app_1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MensajeContacto, Producto, Productor, Cliente
 
 # Register your models here.
+
+admin.site.register(MensajeContacto)
+admin.site.register(Producto)
+admin.site.register(Productor)
+admin.site.register(Cliente)
--- prueba/app_1/models.py
+++ prueba/app_1/models.py
@@ -44,6 +44,22 @@ class Productor(models.Model):
         self.save()
 
     def __str__(self):
-        return self.nombre
+        return self.nombreContacto
     class Meta:
         verbose_name_plural = "Productores"
+        
+class Cliente(models.Model):
+    idCliente = models.AutoField(primary_key=True)
+    nombreCliente = models.CharField(max_length=100, null=True, blank=True)
+    rut_identificador = models.CharField(max_length=15, null=True, blank=True)
+    direccion = models.CharField(max_length=100, null=True, blank=True)
+    comuna = models.CharField(max_length=100, null=True, blank=True)
+    correo = models.CharField(max_length=100, null=True, blank=True)
+    fono = models.CharField(max_length=100, null=True, blank=True)
+    
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
+
+    def __str__(self):
+        return self.nombreCliente
