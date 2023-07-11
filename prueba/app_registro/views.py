5aab9dcdc1e36c35e849bf02191a3cd2d44aa106
Leonardo Rodenas Escobar <leo.rodenas.e@gmail.com>
2023-07-11 11:14:53 -0400

    respaldo enviar correo al registrarse

--- prueba/app_registro/views.py
+++ prueba/app_registro/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.utils.crypto import get_random_string
+from app_1.models import Cliente
 
 def registrar_usuario(request):
     if request.method == 'POST':
         # Obtén los datos del formulario
         username = request.POST['username']
-        password = request.POST['password']
         email = request.POST['email']
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
+        password = get_random_string(length=6)
 
         # Verifica si el usuario ya existe
         if User.objects.filter(username=username).exists():
@@ -21,11 +24,29 @@ def registrar_usuario(request):
             return render(request, 'registro.html', {'error': 'El email ya está en uso.'})
 
         # Crea el nuevo usuario
-        user = User.objects.create_user(
-            username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        User.objects.create_user(
+            username=username, email=email, first_name=first_name, last_name=last_name,  password=password)
+        
+        # Envía correo
+        send_mail(
+            'Somos de Te lo Envío, traemos tu contraseña ♥️',
+            'Hola ' + first_name + ' ' + last_name + ', tu contraseña es: ' + password,
+            'talento@fabricadecodigo.dev',
+            [email],
+            fail_silently=False
+        )
 
         # Redirecciona a una página de éxito o cualquier otra página deseada
         return redirect('login')
 
     # Si es una solicitud GET, muestra el formulario de registro 
     return render(request, 'registro.html')
+
+
+ 
+
+ 
+
+
+
+      
